76d42d67644930da48cfec4d72d215c7647abde3
dingo44kr <gamil.com>
2016-02-01 16:54:40 +0900

    webstorm 2016.02.01

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var path = __dirname;
+  path = path.replace('\\routes','');
+  res.sendFile(path+'/views/index.html')
+
+});
+
+module.exports = router;
