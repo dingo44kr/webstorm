0a1bb8aa0e312d76f3248c88491962e8741485b3
dingo44kr <gamil.com>
2016-02-05 12:58:23 +0900

    last node.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 3,
+  host : 'localhost',
+  user : 'system',
+  database : 'mysql',
+  password : 'mysql'
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var path = __dirname;
+  pool.getConnection(function (err, connection) {
+    connection.query('SELECT * FROM MEMBER', function (err,rows) {
+      if(err){
+        console.log('err :'+err);
+      }
+      console.log('rows: '+JSON.stringify(rows));
+     /* res.render('index', {title : 'MYSQL테스트', rows:rows});*/
+      connection.release();
+    });
+  });
+   var path = __dirname;
   path = path.replace('\\routes','');
   res.sendFile(path+'/views/index.html')
 
